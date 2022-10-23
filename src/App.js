359737550db307250afd90c127af8bf0701214d0
Kevin <86487929+tar83@users.noreply.github.com>
2022-10-23 12:51:42 -0500

    exercise 5.1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import { useState, useEffect } from 'react';
+import Blog from './components/Blog';
+import blogService from './services/blogs';
+import loginService from './services/login';
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [user, setUser] = useState(null);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState(null);
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    if (user !== null) {
+      blogService.getAll(user).then(blogs =>
+        setBlogs(blogs)
+      );
+    }
+  }, [user]);
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const user = await loginService.login({ username, password });
+      setUser(user);
+      setUsername('');
+      setPassword('');
+    } catch (exception) {
+      setMessage('Wrong credentials');
+      setTimeout(() => {
+        setMessage(null);
+      }, 5000);
+    }
+  };
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>log in to application</h2>
+        <form onSubmit={handleLogin}>
+          username: <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+          <br />
+          password: <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+          <br />
+          <button type="submit">login</button>
+        </form>
+      </div>
+    );
+  }
 
   return (
     <div>
       <h2>blogs</h2>
+      <div>
+        {user.name} logged in
+      </div>
+      <br />
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,8 +1,12 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
+const getAll = ({ token }) => {
+  const request = axios.get(baseUrl, {
+    headers: {
+      'Authorization': `bearer ${token}`,
+    },
+  });
   return request.then(response => response.data)
 }
 
