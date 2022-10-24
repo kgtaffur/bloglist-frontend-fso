357d1f76ffe7a08699c84fd62d23ee16447241df
Kevin <86487929+tar83@users.noreply.github.com>
2022-10-24 07:51:44 -0500

    exercise 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,32 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  if (visible) {
+    return (
+      <div className="blog">
+        <div>
+          {blog.title} <button onClick={() => setVisible(!visible)}>hide</button>
+        </div>
+        <div>
+          {blog.url}
+        </div>
+        <div>
+          {blog.likes} <button>like</button>
+        </div>
+        <div>
+          {blog.likes}
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div className="blog">
+      {blog.title} <button onClick={() => setVisible(!visible)}>view</button>
+    </div>
+  );
+};
 
 export default Blog;
