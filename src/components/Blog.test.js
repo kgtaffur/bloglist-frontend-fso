a1303f3b5f3f7ba357c3af0c65b4c01229bd4c35
Kevin <86487929+tar83@users.noreply.github.com>
2022-10-25 11:45:40 -0500

    exercise 5.14

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
 import Blog from './Blog';
+import userEvent from '@testing-library/user-event';
 
 test('renders content', () => {
   const blog = {
@@ -27,5 +28,30 @@ test('renders only title and author by default', () => {
   const div = container.querySelector('.blog');
   expect(div).toHaveTextContent(
     'This is a test title This is a test author'
+  );
+  expect(div).not.toHaveTextContent(
+    'This is a test url'
+  );
+});
+
+test('url and likes are shown when view button has been clicked', async () => {
+  const blog = {
+    title: 'This is a test title',
+    author: 'This is a test author',
+    url: 'This is a test url',
+    likes: 0,
+    user: 'Test user',
+  };
+
+  const mockHandler = jest.fn();
+
+  const { container } = render(<Blog blog={blog} updateBlog={mockHandler} />);
+
+  const button = screen.getByText('view');
+  await userEvent.click(button);
+
+  const div = container.querySelector('.blog');
+  expect(div).toHaveTextContent(
+    'This is a test title hideThis is a test urllikes 0 likeThis is a test authorremove'
   );
 });
